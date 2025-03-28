f80850035386bf0a6d7c587eef2d9cf54cf67a2a
Sai Krishna Vishnumolakala <saikrishna.v1970@gmail.com>
2025-03-28 03:16:00 -0400

    Fixed packaging configuration for PyPI compatibility

--- setup.py
+++ setup.py
@@ -1,41 +1,9 @@
-from setuptools import setup, find_packages
+"""
+This setup.py file is a thin wrapper that defers to setuptools.build_meta
+configured in pyproject.toml. It exists primarily for backward compatibility.
+"""
 
-with open("modelport/README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setup(
-    name="modelport",
-    version="2.0.0",
-    author="Sai Krishna Vishnumolakala",
-    author_email="saikrishna.v1970@gmail.com",
-    description="Export and run ML models anywhere",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/SaiKrishna-KK/model-port",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
-    install_requires=[
-        "torch",
-        "onnx",
-        "onnxruntime",
-        "typer[all]",
-        "numpy",
-    ],
-    entry_points={
-        "console_scripts": [
-            "modelport=modelport.modelport:app",
-        ],
-    },
-    include_package_data=True,
-    package_data={
-        "modelport": ["templates/*", "examples/*"],
-    },
-)
-
-if __name__ == "__main__":
-    setup() 
+# All metadata in pyproject.toml now
+setup() 
